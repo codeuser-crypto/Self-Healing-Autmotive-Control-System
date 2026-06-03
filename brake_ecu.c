99a3f3e199542a1f7b9097320bf4c0fc5d9cbfc2
Bandi Vijay Shreyank <vijayshreyank@gmail.com>
2026-06-03 17:58:59 +0530

    Create brake_ecu.c

---
+++ brake_ecu.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include "vehicle_data.h"
+
+int main()
+{
+    int fd = shm_open("/vehicle_shm", O_RDWR, 0666);
+
+    VehicleData *data = mmap(0, sizeof(VehicleData),
+                              PROT_READ | PROT_WRITE,
+                              MAP_SHARED, fd, 0);
+
+    while(1)
+    {
+    	data->brake_hb = time(NULL);
+        if(data->action == 2)
+        {
+            printf("Brake ECU -> APPLYING EMERGENCY BRAKE\n");
+        }
+
+        sleep(2);
+    }
+
+    return 0;
+}
